9578d9ef8273da65b7ec964f5610d19b45b11ba3
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-06 16:11:03 +0100

    Iteration 2

--- Week 2/variables_and_operators.py
+++ Week 2/variables_and_operators.py
@@ -1,7 +1,12 @@
 # See the week 2 tasks pdf for more information.itemPrice = float(input("Enter the item price: ")) itemTax
 
 itemPrice = float(input("Enter the item price: "))
-itemTax = 8.5
+itemTax = itemPrice * 20 / 100   # or itemPrice * 0.2
+totalPrice = itemPrice + itemTax
 
-totalPrice = itemPrice + (itemPrice * itemTax / 100)
-print("The total price is: ", totalPrice)
+print("The total price (including 20% tax) is:", totalPrice)
+
+numItems = int(input("How many items do you want to buy? "))
+totalCost = totalPrice * numItems
+
+print("The price for", numItems, "items is", format(totalCost, ".2f"))
