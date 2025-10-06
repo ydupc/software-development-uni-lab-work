c1038ba1665144b88029ef756ab561b0ac863c81
Jay Salway <2512956@leedstrinity.ac.uk>
2025-10-06 15:59:09 +0100

    Iteration 1

--- Week 2/variables_and_operators.py
+++ Week 2/variables_and_operators.py
@@ -1,1 +1,7 @@
-# See the week 2 tasks pdf for more information.
+# See the week 2 tasks pdf for more information.itemPrice = float(input("Enter the item price: ")) itemTax
+
+itemPrice = float(input("Enter the item price: "))
+itemTax = 8.5
+
+totalPrice = itemPrice + (itemPrice * itemTax / 100)
+print("The total price is: ", totalPrice)
